ca860fcebcfda5b2bd6d4440fce1e92c792a9f92
Johan Kasperi <johan@kasperi.se>
2014-10-28 12:20:32 +0000

    updates

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var http = require('http')
 var url = require('url')
-var bl = require('bl')
 
 var dabasOpt = {
   hostname: 'api.dabas.com',
@@ -17,86 +16,48 @@ var lcafdOpt = {
   path: '',
 }
 
-dabasSearch = function(key, callback) {
-  dabasOpt.path = '/DABASService/V1/articles/searchparameter/'+key+'/json?apikey='+dabasOpt.apiKey;
-  var req = http.request(dabasOpt, function(res) {
+httpReq = function(opt, article, property, callback, nextfunc) {
+  var req = http.request(opt, function(res) {
     res.setEncoding('utf8');
-    res.pipe(bl(function(err, data) {
-      if(err)
-        return callback(err);
-      dabasGtin(data[0].gtin, callback);
-    }));
+    if(res.statusCode != 200)
+      return callback(null, article);    
+    
+    var result = '';
+    res.on('data', function(data) { 
+      result += data;
+    });
+    res.on('end', function() {
+      article[property] = JSON.parse(result);
+      if(nextfunc == null)
+        return callback(null, article);
+      nextfunc(article, callback);
+    })
   });
   req.on('error', function(e) {
     return callback(e);
   });
-  req.end();
+  req.end();   
 }
+
+/*dabasSearch = function(key, callback) {
+  dabasOpt.path = '/DABASService/V1/articles/searchparameter/'+key+'/json?apikey='+dabasOpt.apiKey;
+  console.log(dabasOpt.path);
+  httpReq(dabasOpt, {}, "dabas", callback, dabasGtin)
+}*/
 
 dabasGtin = function (gtin, callback) {
   dabasOpt.path = '/DABASService/V1/article/gtin/'+gtin+'/json?apikey='+dabasOpt.apiKey;
-  var req = http.request(dabasOpt, function(res) {
-    res.setEncoding('utf8');
-    var result = '';
-    res.on('data', function(data) {
-      result += data;
-    });
-    res.on('end', function() {
-      result = JSON.parse(result);
-      var article = { Artikelbenamning: result.Artikelbenamning }
-      lcafdSearch(article, callback);
-    })
-  });
-  req.on('error', function(e) {
-    return callback(e);
-  });
-  req.end(); 
+  httpReq(dabasOpt, {}, "dabas", callback, lcafdSearch)
 }
 
 lcafdSearch = function (article, callback) {
-  console.log(article);
-  lcafdOpt.path = "/web/api/ingredients?search="+article.Artikelbenamning+"&limit=1";
-  console.log(lcafdOpt.path);
-  var req = http.request(lcafdOpt, function(res) {
-    res.setEncoding('utf8');
-    var result = '';
-    res.on('data', function(data) { 
-      result += data;
-    });
-    res.on('end', function() {
-      result = JSON.parse(result);
-      console.log(result);
-      article["lcafdId"] = result.ingredients[0].id;
-      calculateCarbon(article, callback);
-    })
-  });
-  req.on('error', function(e) {
-    return callback(e);
-  });
-  req.end(); 
+  lcafdOpt.path = "/web/api/ingredients?search="+article.dabas.Artikelbenamning+"&limit=1";
+  httpReq(lcafdOpt, article, "LCAFDdata", callback, calculateCarbon) 
 }
 
 calculateCarbon = function (article, callback) {
-  lcafdOpt.path = "/web/api/func/calculate?ingredient="+article.lcafdId+"&amount="+article.Bruttovikt+"&unit=gram";
-  console.log(lcafdOpt.path);
-  var req = http.request(lcafdOpt, function(res) {
-    res.setEncoding('utf8');
-    var result = '';
-    res.on('data', function(data) { 
-      result += data;
-    });
-    res.on('end', function() {
-      result = JSON.parse(result);
-      console.log(result);
-      article["carbon"] = result.carbon.average;
-      console.log(article.carbon);
-      callback(null, article);
-    })
-  });
-  req.on('error', function(e) {
-    return callback(e);
-  });
-  req.end(); 
+  lcafdOpt.path = "/web/api/func/calculate?ingredient="+article.LCAFDdata.ingredients[0].id+"&amount="+article.Bruttovikt+"&unit=gram";
+  httpReq(lcafdOpt, article, "LCAFDcarbon", callback, null) 
 }
 
 returnResult = function (res, result) {
@@ -111,26 +72,27 @@ returnResult = function (res, result) {
 
 var server = http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true)
-  var search = parsedUrl.query.search;
-  var gtin = parsedUrl.query.gtin;
+  /*var search = parsedUrl.query.search;
   if(search != null) 
     dabasSearch(search, function(err, result) {
       if(err)
          returnResult(res, null);
       returnResult(res, result);
-    });
-  else if(gtin != null)
-    dabasGtin(gtin, function(err, result) {
-      if(err)
-        returnResult(res, null);
-      returnResult(res, result);
-    });
-  else {
-    returnResult(res, null);
+    });*/
+  if (/^\/api\/get/.test(req.url)) {
+    var gtin = parsedUrl.query.gtin;
+    if(gtin != null)
+      dabasGtin(gtin, function(err, result) {
+        if(err)
+          returnResult(res, null);
+        returnResult(res, result);
+      });
+    else {
+      returnResult(res, null);
+    }
   }
-  /*if (/^\/api\/food/.test(req.url))
-    result = parsetime(time)
-  else if (/^\/api\/food/.test(req.url))
-    result = unixtime(time)*/
+  else {
+    returnResult(res, null)
+  }
 })
 server.listen(Number(3333))
